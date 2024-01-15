604ce40ef04272f7a3f2387eef75b212f9dac3bd
raheel0316 <raheelanwaar0316@gmail.com>
2024-01-15 09:19:20 -0800

    middleware

--- app/Http/Middleware/fees.php
+++ app/Http/Middleware/fees.php
@@ -15,17 +15,12 @@ class fees
      */
     public function handle(Request $request, Closure $next): Response
     {
-
-        if (auth()->user()->status == 'approved')
-        {
-        return $next($request);
+        if (auth()->user()->status == 'approved') {
+            return $next($request);
         }
-        }
-        elseif (auth()->user()->status == 'rejected')
-        {
+        if (auth()->user()->status == 'rejected') {
             return redirect()->route('Registeration.Fees')->with('error', 'Your account has been rejected give us correct information');
-        }
-         else {
+        } else {
             return redirect()->url('/')->with('error', 'Your Account is Pending wait for Approval');
         }
     }

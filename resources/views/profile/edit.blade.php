5507ac00c0ac9ef4a9a4d1ce7f0468c5fe6a8cd0
raheel0316 <raheelanwaar0316@gmail.com>
2024-01-29 09:34:00 -0800

    profile password can be changed

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -101,6 +101,52 @@
             </form>
         </div>
     </div>
+    <h3 class="text-center">Update Your Password</h3>
+    <div class="container">
+        <div class="card bg-warning">
+            <div class="card-body">
+                <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+                    @csrf
+                    @method('put')
+                    <div>
+                        <x-input-label for="update_password_current_password" :value="__('Current Password')" />
+                        <br>
+                        <x-text-input id="update_password_current_password" name="current_password" type="password"
+                            class="mt-1 block w-full" autocomplete="current-password" />
+                        <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="update_password_password" :value="__('New Password')" />
+                        <br>
+                        <x-text-input id="update_password_password" name="password" type="password"
+                            class="mt-1 block w-full" autocomplete="new-password" />
+                        <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
+                        <br>
+                        <x-text-input id="update_password_password_confirmation" name="password_confirmation"
+                            type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                        <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center gap-4">
+                        <button type="submit" class="btn btn-dark mt-3">Update</button>
+                        @if (session('status') === 'password-updated')
+                            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                                class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
+                        @endif
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
 
     <!-- Add Bootstrap JS and Popper.js scripts (optional but recommended) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>

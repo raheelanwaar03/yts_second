596b227c712bf6de052c69777313a611a7656d9c
raheel0316 <raheelanwaar0316@gmail.com>
2024-03-21 11:36:20 -0700

    asdf

--- resources/views/admin/user/allUsers.blade.php
+++ resources/views/admin/user/allUsers.blade.php
@@ -53,9 +53,7 @@
                                                                     <td>{{ $item->trxIds->sender_number ?? 'null' }}</td>
                                                                     <td>{{ $item->trxIds->trx_id ?? 'null' }}</td>
                                                                     <td>
-                                                                        <img src="{{ asset('images/' . $item->trxIds->screen_shot ?? '1234') }}"
-                                                                            alt="Null" class="img-fluid" height="100px"
-                                                                            width="100px">
+                                                                        null
                                                                     </td>
                                                                     <td>
                                                                         <a href="{{ route('Admin.Approve.User', $item->id) }}"
